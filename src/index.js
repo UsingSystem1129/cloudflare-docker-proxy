319dc54a28a0691a4ee88665f4e70c1dc3c64bd1
UsingSystem1129 <515701275@qq.com>
2024-08-08 15:12:14 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.msai.site": dockerHub,
+  "quay.msai.site": "https://quay.io",
+  "gcr.msai.site": "https://gcr.io",
+  "k8s-gcr.msai.site": "https://k8s.gcr.io",
+  "k8s.msai.site": "https://registry.k8s.io",
+  "ghcr.msai.site": "https://ghcr.io",
+  "cloudsmith.msai.site": "https://docker.cloudsmith.io",
+  "ecr.msai.site": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.msai.site": dockerHub,
 };
 
 function routeByHosts(host) {
